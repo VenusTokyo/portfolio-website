08ad47fac32546ab8ef379c28405a96fc937d70f
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2023-01-31 17:43:53 +0530

    :tada:

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Landing.css'
+const Landing = () => {
+  return (
+    <div className='blob-scene landing-blob'>
+      <div className="heading">
+        <ul className='dynamic-txt'>
+            <li><span>Hello</span></li>
+            <li><span>नमस्ते</span></li>
+            <li><span>Bonjour</span></li>
+            <li><span>Hola</span></li>
+            <li><span>শুভেচ্ছা!</span></li>
+
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Landing
