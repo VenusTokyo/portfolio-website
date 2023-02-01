de349d47fd2542203a29e92cdf37cc46c5e40696
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2023-02-01 23:03:05 +0530

    typewriter animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Landing from './components/Landing/Landing';
+
 function App() {
   return (
     <div className="App">
       <Landing/>
+      
     </div>
   );
 }
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import './Landing.css'
 const Landing = () => {
-  return (
-    <div className='blob-scene landing-blob'>
-      <div className="heading">
-        <ul className='dynamic-txt'>
-            <li><span>Hello</span></li>
-            <li><span>नमस्ते</span></li>
-            <li><span>Bonjour</span></li>
-            <li><span>Hola</span></li>
-            <li><span>শুভেচ্ছা!</span></li>
+    return (
+        <div className='blob-scene landing-blob'>
+            <div className="heading">
+                
+                <div className="container">
+                <p className='txt dynamic-txt'>Hello</p>
+                <p className='txt dynamic-txt'>नमस्ते</p>
+                <p className='txt dynamic-txt'>Bonjour</p>
+                <p className='txt dynamic-txt'>Hola</p>
+                <p className='txt dynamic-txt'>শুভেচ্ছা!</p>
+                </div>
+            
 
-        </ul>
-      </div>
-    </div>
-  )
+
+            </div>
+        </div>
+    )
 }
 
 export default Landing
